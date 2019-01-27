ba56f29b59ba12f078723550e0b214315b9cf1f7
Henry Li <zbhenryli@gmail.com>
2019-01-27 11:02:54 -0500

    2 neurons and output function logic okay

--- main.py
+++ main.py
@@ -2,14 +2,27 @@ import csv
 import numpy as np
 import pprint
 
-trainingData = np.loadtxt('testSeeds.csv', delimiter=',')
-normalized = trainingData / trainingData.max(axis=0)
+trainingData = np.loadtxt('testSeeds.csv', delimiter=',') # read csv file
+normalized = trainingData / trainingData.max(axis=0) # normalize data
 
-# def importCSV(filename):
-#     with open (filename, newline='') as csvfile:
-#         reader = csv.reader(csvfile, delimiter=',')
-#         for row in reader:
-#             trainingData.append([float(r) for r in row])
+def yeetWheat(bias, input, weights):
+    # add bias for neuron to determine it's own 
+    output = bias * weights[0]
+    output += sum([x * w for x, w in zip(input[:-1], weights[1:])])
+    print(output)
+    return 1 if output > 0 else 0
 
-# importCSV('testSeeds.csv')
-pprint.pprint(normalized)
+def perceptron():
+    bias = 1
+    input = [1, 2, 3, 4, 5, 6, 7, 8]
+    weights1 = [-0.1, 1, 0.1, 1, 0.1, -1, 0.1, -1]
+    weights2 = [-1, 0.1, 1, 0.1, 1, 0.1, 1, 0.1]
+
+    y1 = yeetWheat(bias, input, weights1)
+    y2 = yeetWheat(bias, input, weights2)
+    return str(y1) + str(y2)
+
+# pprint.pprint(trainingData)
+# pprint.pprint(normalized)
+
+print(perceptron())
