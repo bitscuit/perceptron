a359ff1f56e44cc64fd731dd12a96226d15eec5c
Henry Li <zbhenryli@gmail.com>
2019-01-27 13:35:56 -0500

    added main function and input and weights are passed to training function

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ trainingData[:, :-1] = normalized
 
 ITERATIONS = 3
 
+
 def yeetWheat(bias, input, weights):
     # add bias for neuron to determine it's own 
     output = bias * weights[0]
@@ -15,17 +16,20 @@ def yeetWheat(bias, input, weights):
     print(output)
     return 1 if output > 0 else 0
 
-def perceptron():
+
+def perceptron(input, weights1, weights2):
     bias = 1
-    input = [1, 2, 3, 4, 5, 6, 7, 8]
-    weights1 = [-0.1, 1, 0.1, 1, 0.1, -1, 0.1, -1]
-    weights2 = [-1, 0.1, 1, 0.1, 1, 0.1, 1, 0.1]
 
     y1 = yeetWheat(bias, input, weights1)
     y2 = yeetWheat(bias, input, weights2)
     return str(y1) + str(y2)
 
-def wheatifyPerceptron():
+
+def updateWeights(input, weights):
+    return weights
+
+
+def wheatifyPerceptron(input, weights1, weights2):
     # loop over iterations
         # loop over dataset
             # perceptron on a single row
@@ -34,9 +38,22 @@ def wheatifyPerceptron():
         # check how many were correctly classified
         # maybe stop if accurate enough
         # maybe update weights
-    return 'weights'
+    weights1 = updateWeights(input, weights1)
+    weights2 = updateWeights(input, weights2)
+    print(perceptron(input, weights1, weights2))
+    return 'finalized weights'
 
-pprint.pprint(trainingData)
-# pprint.pprint(normalized[:-1])
 
-print(perceptron())
+def main():
+    pprint.pprint(trainingData)
+    # pprint.pprint(normalized[:-1])
+
+    input = [1, 2, 3, 4, 5, 6, 7, 8]
+    weights1 = [-0.1, 1, 0.1, 1, 0.1, -1, 0.1, -1]
+    weights2 = [-1, 0.1, 1, 0.1, 1, 0.1, 1, 0.1]
+
+    print(wheatifyPerceptron(input, weights1, weights2))
+
+
+if __name__ == '__main__':
+    main()
